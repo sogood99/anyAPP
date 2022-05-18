0f14f6b8eca6a49067b34d6c74de2f33372a0313
Onii Chan <46487694+sogood99@users.noreply.github.com>
2022-05-19 00:03:43 +0800

    Added in homepage top appbar

--- app/src/main/java/com/example/anyapp/Home.kt
+++ app/src/main/java/com/example/anyapp/Home.kt
@@ -2,10 +2,33 @@ package com.example.anyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.android.material.appbar.MaterialToolbar
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // For selecting the Menu Items
+        val topAppBar = findViewById<MaterialToolbar>(R.id.homeToolbar)
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.miSearch -> {
+                    Log.v("Pity", "Clicked Search")
+                    true
+                }
+                else -> true
+            }
+        }
+
+        // For selecting the Home
+        val homeButton = findViewById<Button>(R.id.homeButton)
+        homeButton.setOnClickListener { button ->
+            Log.v("Pity", "Clicked Home Button")
+            true
+        }
+
     }
 }
