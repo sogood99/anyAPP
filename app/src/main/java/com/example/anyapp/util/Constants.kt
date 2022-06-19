eaa6877e19372bd677793c6d67d162d0eb1d032c
realdoonser <transfors@outlook.com>
2022-06-19 01:38:35 -0500

    update Constants for mobile record

--- app/src/main/java/com/example/anyapp/util/Constants.kt
+++ app/src/main/java/com/example/anyapp/util/Constants.kt
@@ -11,7 +11,7 @@ enum class FeedType {
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://10.0.2.2:8000"
+        const val BASE_URL = "http://192.168.1.7:8000"
     }
 }
 
